e78662fe729d484e6ff096f3fc80d29f744bedf5
nozystem <nozystem@gmail.com>
2023-02-17 17:00:01 +0100

    [feat] make some changes

--- main.java
+++ main.java
@@ -19,6 +19,7 @@ public class main {
        Casa miCasa = new Casa("53317286C", "sergio", 21);
         System.out.println(miCasa.getNombre());/*
          */
+        
         ArrayList<Casa> casas = new ArrayList<>();
         BufferedReader terminal = new BufferedReader(new InputStreamReader(System.in));
         boolean quit = false;
@@ -28,18 +29,28 @@ public class main {
             String[] palabra = leer.split(" "); // Hago un split de leer y lo guardo en una array list palabra
             switch (palabra[0]) {  // Al switch le paso como parametro la primera palabra de la array
                 case "addCasa":
+                    
                     if (palabra.length == 4){
-                        Casa miCasa = new Casa(palabra[1], palabra[2], Integer.parseInt(palabra[3]));
-                        casas.add(miCasa);
-                    }
+                        if (Integer.parseInt(palabra[3]) < 10) {System.out.println("No tienes suficiente espacio para poner placas");}
+                        else if (palabra[1].length() != 9) {System.out.println("Introduce un NIF valido");}
+                        else{
+                            Casa miCasa = new Casa(palabra[1], palabra[2], Integer.parseInt(palabra[3]));
+                            casas.add(miCasa);
+                            System.out.println("OK: Casa registrada.");
+                        }
+                    }break;
+                    case "list":
+                    System.out.println("Cases enregistrades: " + casas.size());
+                    for( int i = 0; i < casas.size(); i ++){
+                        System.out.println("Client: " + casas.get(i).getNif() + " - " + casas.get(i).getNombre());
+                        System.out.println("Superfície de teulada: " + casas.get(i).getSuperficie());}
                     break;
+                    
                 case "quit":
                     quit = true;
                     break;
             
-                case "list":
-                    for( int i = 0; i > casas.length(); i ++)
-                    break;
+
             }
         } while (!quit); // quit == false
     }
